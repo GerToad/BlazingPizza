b6924f884eac9675f053d594651f8cd030c1ca21
Ger Toad <theyayo47@gmail.com>
2023-04-01 13:19:21 -0600

    program in server

--- HelloBlazor/Client/obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
+++ HelloBlazor/Client/obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
@@ -13,77 +13,77 @@ namespace HelloBlazor.Client.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\WM\source\repos\HelloBlazor\HelloBlazor\Client\_Imports.razor"
+#line 1 "C:\Users\WM\source\repos\BlazingPizza\HelloBlazor\Client\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\WM\source\repos\HelloBlazor\HelloBlazor\Client\_Imports.razor"
+#line 2 "C:\Users\WM\source\repos\BlazingPizza\HelloBlazor\Client\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\WM\source\repos\HelloBlazor\HelloBlazor\Client\_Imports.razor"
+#line 3 "C:\Users\WM\source\repos\BlazingPizza\HelloBlazor\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\WM\source\repos\HelloBlazor\HelloBlazor\Client\_Imports.razor"
+#line 4 "C:\Users\WM\source\repos\BlazingPizza\HelloBlazor\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\WM\source\repos\HelloBlazor\HelloBlazor\Client\_Imports.razor"
+#line 5 "C:\Users\WM\source\repos\BlazingPizza\HelloBlazor\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\WM\source\repos\HelloBlazor\HelloBlazor\Client\_Imports.razor"
+#line 6 "C:\Users\WM\source\repos\BlazingPizza\HelloBlazor\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\WM\source\repos\HelloBlazor\HelloBlazor\Client\_Imports.razor"
+#line 7 "C:\Users\WM\source\repos\BlazingPizza\HelloBlazor\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\WM\source\repos\HelloBlazor\HelloBlazor\Client\_Imports.razor"
+#line 8 "C:\Users\WM\source\repos\BlazingPizza\HelloBlazor\Client\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\WM\source\repos\HelloBlazor\HelloBlazor\Client\_Imports.razor"
+#line 9 "C:\Users\WM\source\repos\BlazingPizza\HelloBlazor\Client\_Imports.razor"
 using HelloBlazor.Client;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\WM\source\repos\HelloBlazor\HelloBlazor\Client\_Imports.razor"
+#line 10 "C:\Users\WM\source\repos\BlazingPizza\HelloBlazor\Client\_Imports.razor"
 using HelloBlazor.Client.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\WM\source\repos\HelloBlazor\HelloBlazor\Client\_Imports.razor"
+#line 11 "C:\Users\WM\source\repos\BlazingPizza\HelloBlazor\Client\_Imports.razor"
 using HelloBlazor.Shared;
 
 #line default
@@ -98,7 +98,7 @@ using HelloBlazor.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 22 "C:\Users\WM\source\repos\HelloBlazor\HelloBlazor\Client\Pages\Index.razor"
+#line 22 "C:\Users\WM\source\repos\BlazingPizza\HelloBlazor\Client\Pages\Index.razor"
       
     List<PizzaSpecial> Specials;
     protected async override Task OnInitializedAsync()
